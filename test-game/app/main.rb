2b7ce22695012514a967e4eb1bc1a55b54b348c3
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-02-25 14:46:19 +0900

    Add a simple build and run on device

--- test-game/app/main.rb
+++ test-game/app/main.rb
@@ -1,3 +1,24 @@
 require 'lib/jni'
 
-# Empty app/main.rb for running tests
+def tick(args)
+  if $gtk.platform? :android
+    if Kernel.tick_count.zero?
+      run_jni_tests(args)
+    else
+      render_jni_test_results(args)
+    end
+  else
+    args.outputs.labels << {
+      x: 640, y: 360, text: 'This example only works on Android.', size_enum: 2,
+      alignment_enum: 1, vertical_alignment_enum: 1
+    }
+  end
+end
+
+def run_jni_tests(args)
+
+end
+
+def render_jni_test_results(args)
+
+end
