6267eba9f9883c72bd083e49a28c354811538cd7
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-02-25 15:10:24 +0900

    On device test basic

--- test-game/app/main.rb
+++ test-game/app/main.rb
@@ -15,10 +15,29 @@ def tick(args)
   end
 end
 
-def run_jni_tests(args)
+TESTS = {}
+TESTS['Access Game Activity'] = lambda do
+end
 
+def run_jni_tests(args)
+  results = {}
+  TESTS.each do |name, test|
+    test.call
+    results[name] = 'OK'
+  rescue StandardError => e
+    results[name] = "FAIL: #{e}"
+    puts e
+  end
+  args.state.results = results
 end
 
 def render_jni_test_results(args)
-
+  results = args.state.results
+  y = 1260
+  results.each do |name, result|
+    args.outputs.labels << {
+      x: 20, y: y, text: "#{name}: #{result}", size_enum: 2
+    }
+    y -= 20
+  end
 end
