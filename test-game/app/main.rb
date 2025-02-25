583639be89a051e43138bffb0e2c3bd0b94c4cfb
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-02-25 16:22:28 +0900

    Improve test running and output

--- test-game/app/main.rb
+++ test-game/app/main.rb
@@ -22,22 +22,32 @@ end
 def run_jni_tests(args)
   results = {}
   TESTS.each do |name, test|
+    puts "========== #{name} =========="
     test.call
     results[name] = 'OK'
   rescue StandardError => e
     results[name] = "FAIL: #{e}"
-    puts e
+    puts "  #{e.message} (#{e.class})"
+  ensure
+    puts '=' * (22 + name.length)
+    puts ' '
   end
   args.state.results = results
 end
 
 def render_jni_test_results(args)
+  args.outputs.background_color = { r: 0x00, g: 0x00, b: 0x00 }
   results = args.state.results
   y = 1260
   results.each do |name, result|
+    success = result == 'OK'
+    color = success ? { r: 0x62, g: 0x9e, b: 0x51 } : { r: 0xbf, g: 0x1b, b: 0x00 }
     args.outputs.labels << {
-      x: 20, y: y, text: "#{name}: #{result}", size_enum: 2
+      x: 20, y: y, text: "#{name}:", size_enum: 2, **color
     }
-    y -= 20
+    args.outputs.labels << {
+      x: 700, y: y, text: result, size_enum: 2, alignment_enum: 2, **color
+    }
+    y -= 25
   end
 end
